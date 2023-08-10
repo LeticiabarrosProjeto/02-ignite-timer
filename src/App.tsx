18940ba887c1dbe0b748eb4a957ed5e6e2fb39cb
leticiabarrosProjeto <leticiabaarros97@gmail.com>
2023-08-10 14:02:13 -0300

    Configurando temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { ThemeProvider } from 'styled-components'
 import { Button } from "./componets/Button"
+
+import { defaultTheme } from './componets/styles/themes/default'
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
     <Button variant= "primary" />
     <Button variant= "secondary" />
     <Button variant= "success" />
     <Button variant= "danger" />
     <Button />
-    </>
+    </ThemeProvider>
 )
 }
